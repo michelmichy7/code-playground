965e83cd0b6d8a2b7b5acb7031e694a58311745e
Michael <michelmichy7@gmail.com>
2025-04-26 22:36:48 +0100

    this will read all files in folder + add them to list and print

--- Cpp/filesystems/main.cpp
+++ Cpp/filesystems/main.cpp
@@ -2,18 +2,26 @@
 using namespace std;
 
 #include <filesystem>
+#include <deque>
 
 int main() {
     filesystem::path p = "dir";
+
+    deque<string> files = {};
 
     cout << "Path: " << p << endl;
 
 
     if (filesystem::exists(p)) {
-        cout << "Exists"; 
+        cout << "Exists \n"; 
     }
     
     for (const auto& entry:filesystem::directory_iterator(p)) {
-        cout << entry.path() << endl;
+        files.push_back(entry.path().string());
     }
+
+    for (int i = 0; i < files.size(); i++) {
+        cout << files[i] << endl;
+    }
+    
 }
