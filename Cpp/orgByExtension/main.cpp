61f21e964cf03de33b456b532bea91467dd94796
Michal <michelmichy7@gmail.com>
2025-05-01 12:37:35 +0200

    now organizing files only in list, not files yet in folders

--- Cpp/orgByExtension/main.cpp
+++ Cpp/orgByExtension/main.cpp
@@ -8,12 +8,27 @@ namespace fs = std::filesystem;
 
 
 int main() {
-  fs::path path = "./Files/";
+  fs::path path = "./Files";
 
 
     multimap<string, string> filesList;
+    multimap<string, string> organizedFiles;
 
-  for (const auto& entry : fs::directory_iterator(path)) {
-    cout << "File: " << entry.path().extension().string << endl;
+    for (const auto& entry : fs::directory_iterator(path)) {
+      filesList.emplace(entry.path().filename(), entry.path().extension().string());
+    }
+
+    for (const auto& file : filesList) {
+      if (file.second == ".pdf" || file.second == ".txt") {
+        organizedFiles.emplace(file.first, "Documents");
+      }
+
+      else if (file.second == ".mp3" || file.second == ".wav" || file.second == ".m4a") {
+        organizedFiles.emplace(file.first, "Music");
+      }
+
+      for (const auto& orgFile : organizedFiles) {
+        cout << orgFile.first << ", " << orgFile.second << endl;
+      }
+    }
   }
-}
