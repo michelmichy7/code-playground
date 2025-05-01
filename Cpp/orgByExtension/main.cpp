0f076ce1b8aad3355d010b6b6e1783f7d19ac2f4
Michael <michelmichy7@gmail.com>
2025-05-01 16:54:44 +0100

    .

--- Cpp/orgByExtension/main.cpp
+++ Cpp/orgByExtension/main.cpp
@@ -15,16 +15,22 @@ int main() {
     multimap<string, string> organizedFiles;
 
     for (const auto& entry : fs::directory_iterator(path)) {
-      filesList.emplace(entry.path().filename(), entry.path().extension().string());
+      filesList.emplace(entry.path().filename().string(), entry.path().extension().string());
     }
 
     for (const auto& file : filesList) {
       if (file.second == ".pdf" || file.second == ".txt") {
         organizedFiles.emplace(file.first, "Documents");
+
+        create_directory(path / "Documents");
+        copy_file(path / file.first, path / "Documents" / file.first);
       }
 
       else if (file.second == ".mp3" || file.second == ".wav" || file.second == ".m4a") {
         organizedFiles.emplace(file.first, "Music");
+
+        create_directory(path / "Music");
+        copy_file(path / file.first, path / "Music" / file.first);
       }
 
       for (const auto& orgFile : organizedFiles) {
